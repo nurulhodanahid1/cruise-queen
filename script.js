9818dc180f572e394e6364f34fcb95263b7a12e6
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-06-20 02:32:43 +0600

    completed the project

---
+++ script.js
@@ -0,0 +1,85 @@
+// part 1
+// document.getElementById('first-class-increase').addEventListener('click', function(){
+//     const firstClassInput = document.getElementById('first-class-count');
+//     const firstClassCount = parseInt(firstClassInput.value);
+//     const firstClassNewCount = firstClassCount + 1;
+//     firstClassInput.value = firstClassNewCount;
+// })
+// document.getElementById('first-class-decrease').addEventListener('click', function(){
+//     const firstClassInput = document.getElementById('first-class-count');
+//     const firstClassCount = parseInt(firstClassInput.value);
+//     const firstClassNewCount = firstClassCount - 1;
+//     firstClassInput.value = firstClassNewCount;
+// })
+
+//  /  /  /   With function   /  /  /  //
+// part 2
+// document.getElementById('first-class-increase').addEventListener('click', function(){
+//     handleTicketBook(true);
+// })
+// document.getElementById('first-class-decrease').addEventListener('click', function(){
+//     handleTicketBook(false);
+// })
+
+// function handleTicketBook(isIncrease){
+//         const firstClassInput = document.getElementById('first-class-count');
+//         const firstClassCount = parseInt(firstClassInput.value);
+//         let firstClassNewCount = firstClassCount;
+//         if(isIncrease == true){
+//             firstClassNewCount = firstClassCount + 1;
+//         }
+//         if(isIncrease == false && firstClassNewCount > 0){
+//             firstClassNewCount = firstClassCount - 1;
+//         }
+//         firstClassInput.value = firstClassNewCount;
+// }
+
+// part - 3   // main part // // // //
+document.getElementById('form-confirmation').style.display = 'none';
+document.getElementById('first-class-increase').addEventListener('click', function(){
+    handleTicketBook('first-class', true);
+})
+document.getElementById('first-class-decrease').addEventListener('click', function(){
+    handleTicketBook('first-class', false);
+})
+
+function handleTicketBook(product, isIncrease){
+        const firstClassInput = document.getElementById(product + '-count');
+        const firstClassCount = parseInt(firstClassInput.value);
+        let firstClassNewCount = firstClassCount;
+        if(isIncrease == true){
+            firstClassNewCount = firstClassCount + 1;
+        }
+        if(isIncrease == false && firstClassNewCount > 0){
+            firstClassNewCount = firstClassCount - 1;
+        }
+        firstClassInput.value = firstClassNewCount;
+
+        calculateTotal();
+}
+
+function calculateTotal(){
+    const economyInput = document.getElementById('economy-count');
+    const economyCount = parseInt(economyInput.value);
+
+    const firstClassInput = document.getElementById('first-class-count');
+    const firstClassCount = parseInt(firstClassInput.value);
+
+    const totalPrice = economyCount * 100 + firstClassCount * 150;
+    document.getElementById('sub-total').innerText = '$' + totalPrice;
+    
+    const tax = Math.round(totalPrice * 0.1);
+    const totalAmount = tax + totalPrice;
+
+    document.getElementById('tax-amount').innerText = '$' + tax;
+    document.getElementById('total-amount').innerText = '$' + totalAmount;
+}
+
+document.getElementById('book-now').addEventListener("click", function(){
+    document.getElementById('form-confirmation').style.display = 'block';
+    document.getElementById('order-form').style.display = 'none';
+})
+document.getElementById('book-again').addEventListener("click", function(){
+    document.getElementById('form-confirmation').style.display = 'none';
+    document.getElementById('order-form').style.display = 'block';
+})
