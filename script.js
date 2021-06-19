8a6fbac8cbec2648150dc56ec44c2e05d0bf49a3
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-06-20 02:44:22 +0600

    successfully done the assignment

--- script.js
+++ script.js
@@ -44,16 +44,16 @@ document.getElementById('first-class-decrease').addEventListener('click', functi
 })
 
 function handleTicketBook(product, isIncrease){
-        const firstClassInput = document.getElementById(product + '-count');
-        const firstClassCount = parseInt(firstClassInput.value);
-        let firstClassNewCount = firstClassCount;
+        const ticketInput = document.getElementById(product + '-count');
+        const ticketCount = parseInt(ticketInput.value);
+        let newCount = ticketCount;
         if(isIncrease == true){
-            firstClassNewCount = firstClassCount + 1;
+            newCount = ticketCount + 1;
         }
-        if(isIncrease == false && firstClassNewCount > 0){
-            firstClassNewCount = firstClassCount - 1;
+        if(isIncrease == false && newCount > 0){
+           newCount = ticketCount - 1;
         }
-        firstClassInput.value = firstClassNewCount;
+        ticketInput.value = newCount;
 
         calculateTotal();
 }
@@ -62,10 +62,10 @@ function calculateTotal(){
     const economyInput = document.getElementById('economy-count');
     const economyCount = parseInt(economyInput.value);
 
-    const firstClassInput = document.getElementById('first-class-count');
-    const firstClassCount = parseInt(firstClassInput.value);
+    const ticketInput = document.getElementById('first-class-count');
+    const ticketCount = parseInt(ticketInput.value);
 
-    const totalPrice = economyCount * 100 + firstClassCount * 150;
+    const totalPrice = economyCount * 100 + ticketCount * 150;
     document.getElementById('sub-total').innerText = '$' + totalPrice;
     
     const tax = Math.round(totalPrice * 0.1);
